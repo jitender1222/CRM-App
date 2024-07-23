c57b7496567539912cf0cc9f974fc8a910887c87
jitender1222 <jeetusinghkanyal@gmail.com>
2024-07-23 19:16:39 +0530

    added our first async thunk for login

--- src/Redux/Slices/auth.js
+++ src/Redux/Slices/auth.js
@@ -1,17 +1,40 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import axiosInstance from "../../config/axiosInstance"
 
 const initialState={
     role: localStorage.getItem("role") || "",
-    data: localStorage.getItem("data") || {},
+    data: JSON.parse(localStorage.getItem("data")) || undefined,
+    token:localStorage.getItem("token") || "",
     isLoggedIn: localStorage.getItem("isLoggedIn") || false
 }
 
+export const login=createAsyncThunk("/auth/login",async (data)=>{
 
+    try {
+        const response= await axiosInstance.post("auth/signin",data);
+        return response; 
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 const authSlice=createSlice({
     name:"auth",
     initialState,
-    reducers:{}
+    reducers:{},
+    extraReducers: (builder) => {
+        builder.addCase(login.fulfilled,(state,action)=>{
+            state.isLoggedIn=(action.payload.data ?.token != undefined);
+            state.data=action.payload.data ?.userData;
+            state.role=action.payload.data ?.userData ?.userType;
+            state.token=action.payload.data ?.token;
+
+            localStorage.setItem("role",action.payload.data ?.userData ?.userType);
+            localStorage.setItem("isLoggedIn",(action.payload.data ?.token != undefined));
+            localStorage.setItem("data",JSON.stringify(action.payload.data ?.userData));
+            localStorage.setItem("token",action.payload.data ?.token);
+        })
+    }
 })
 
 
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,4 +1,31 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../../Redux/Slices/auth";
+
 const Login = () => {
+
+  const dispatch=useDispatch();
+
+  const [loginDetails,setLoginDetails]=useState({
+    email:"",
+    password:""
+  });
+
+  function onSubmit(){
+    console.log("inside")
+    if(!loginDetails.email || !loginDetails.password) return;
+    const response=dispatch(login(loginDetails))
+    console.log(response);
+  }
+
+  function onHandleChange(e){
+    const {name,value}=e.target;
+    console.log(name,value);
+    setLoginDetails({
+      ...loginDetails,
+      [name]: value
+    });
+  }
   return (
     <>
       <div className="flex justify-center h-screen items-center">
@@ -6,22 +33,26 @@ const Login = () => {
           <div className="card-body text-white flex-col	justify-around	">
           <span className="card-title text-3xl justify-center">Login</span>
             <input
+            name="email"
               type="email"
               placeholder="name@gmail.com"
               className="input input-bordered input-warning w-full max-w-xs"
+              onChange={onHandleChange}
             />
 
             <input
+            name="password"
               type="password"
               placeholder="password"
               className="input input-bordered input-warning w-full max-w-xs"
+              onChange={onHandleChange}
             />
-            <button className="btn  btn-warning">Login</button>
+            <button onClick={onSubmit} className="btn  btn-warning">Login</button>
           </div>
         </div>
       </div>
     </>
-  );
+  );  
 };
 
 export default Login;
